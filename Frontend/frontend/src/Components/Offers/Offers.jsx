b302871420b196119c7973508577f93ead60377f
Rutuja-coder08 <68146768+Rutuja-coder08@users.noreply.github.com>
2023-12-23 11:31:38 +0530

    'popular'

---
+++ Frontend/frontend/src/Components/Offers/Offers.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Offers.css";
+
+export const Offers = () => {
+  return <div className="offers"></div>;
+};
--- Frontend/frontend/src/Pages/Shop.jsx
+++ Frontend/frontend/src/Pages/Shop.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { Hero } from "../Components/Hero/Hero";
+import { Popular } from "../Components/Popular/Popular";
 
 // HomePage
 export const Shop = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Hero />
+      <Popular />
+    </div>
+  );
 };
